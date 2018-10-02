fa070254cc59efb37a354acf4490e932cd923893
Ryota Kameoka <kameoka.ryota@gmail.com>
2018-10-03 02:24:03 +0900

    Format code with Prettier

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,11 @@ export {
     ViewInput,
 } from './types';
 
-export function isolate<State extends object, Scope extends Lens<State, any> | keyof State>(scope: Scope): (child: Component<Sources<FormDeclaration<ZoomIn<State, Scope>>>, Sinks<FormDeclaration<ZoomIn<State, Scope>>>>) => IsolatedForm<State, ZoomIn<State, Scope>> {
+export function isolate<State extends object, Scope extends Lens<State, any> | keyof State>(
+    scope: Scope,
+): (
+    child: Component<Sources<FormDeclaration<ZoomIn<State, Scope>>>, Sinks<FormDeclaration<ZoomIn<State, Scope>>>>,
+) => IsolatedForm<State, ZoomIn<State, Scope>> {
     if (typeof scope === 'string') {
         return toIsolated(scope) as any;
     } else {
@@ -46,7 +50,9 @@ export function isolate<State extends object, Scope extends Lens<State, any> | k
     }
 }
 
-export function form<Decl extends FormDeclaration<any>>(fields: FieldsFor<Decl>): Component<Sources<Decl>, Sinks<Decl>> {
+export function form<Decl extends FormDeclaration<any>>(
+    fields: FieldsFor<Decl>,
+): Component<Sources<Decl>, Sinks<Decl>> {
     return function Form({
         DOM,
         onion,
@@ -62,14 +68,20 @@ export function form<Decl extends FormDeclaration<any>>(fields: FieldsFor<Decl>)
                 isolatedDOMSource.events('change'),
                 isolatedDOMSource.events('focus'),
                 isolatedDOMSource.events('input'),
-            ).take(1).addListener({
-                next(_) {
-                    touchedKeys.add(key);
-                },
-            });
+            )
+                .take(1)
+                .addListener({
+                    next(_) {
+                        touchedKeys.add(key);
+                    },
+                });
         });
 
-        const combined$: Stream<[Decl, FormRenderer<Decl>, ValidatorsFor<Decl>]> = Stream.combine(onion.state$, renderer$, validators$);
+        const combined$: Stream<[Decl, FormRenderer<Decl>, ValidatorsFor<Decl>]> = Stream.combine(
+            onion.state$,
+            renderer$,
+            validators$,
+        );
 
         const reducer$s: Stream<Endo<Values<Decl>>>[] = Object.keys(fields).map((key: keyof Decl) => {
             const field: Field<Decl[keyof Decl]> | null | undefined = fields[key];
@@ -82,60 +94,79 @@ export function form<Decl extends FormDeclaration<any>>(fields: FieldsFor<Decl>)
             const domSource = (field as any).shouldNotIsolate ? DOM : isolateSource(DOM, key);
             const endo$ = intent(domSource);
 
-            return endo$.map((endo: Endo<Values<Decl>>) => evolveC<Values<Decl>>({
-                [key]: endo,
-            } as any));
+            return endo$.map((endo: Endo<Values<Decl>>) =>
+                evolveC<Values<Decl>>({
+                    [key]: endo,
+                } as any),
+            );
         });
         const reducer$: Stream<Endo<Values<Decl>>> = Stream.merge(...reducer$s);
 
-        const vnode$: MemoryStream<VNode> = combined$.map(([values, renderer, validators]) => {
-            const errors: Record<keyof Decl, string | null> = Object.keys(fields).map<[keyof Decl, string | null]>((key: keyof Decl) => {
-                const field: Field<Decl[keyof Decl]> | null | undefined = fields[key];
-                const validator: any = validators[key];
+        const vnode$: MemoryStream<VNode> = combined$
+            .map(([values, renderer, validators]) => {
+                const errors: Record<keyof Decl, string | null> = Object.keys(fields)
+                    .map<[keyof Decl, string | null]>((key: keyof Decl) => {
+                        const field: Field<Decl[keyof Decl]> | null | undefined = fields[key];
+                        const validator: any = validators[key];
 
-                if (!field || !validator) {
-                    return [key, null];
-                }
+                        if (!field || !validator) {
+                            return [key, null];
+                        }
 
-                const value = values[key];
-                const error = validator ? validator(value) : null;
+                        const value = values[key];
+                        const error = validator ? validator(value) : null;
 
-                return [key, error];
-            }).reduce((acc: Record<keyof Decl, string | null>, [key, error]: [keyof Decl, string | null]) => Object.assign({}, acc, { [key]: error }), {} as Record<keyof Decl, string | null>);
+                        return [key, error];
+                    })
+                    .reduce(
+                        (acc: Record<keyof Decl, string | null>, [key, error]: [keyof Decl, string | null]) =>
+                            Object.assign({}, acc, { [key]: error }),
+                        {} as Record<keyof Decl, string | null>,
+                    );
 
-            const allValid = Object.values(errors).every(e => e === null);
+                const allValid = Object.values(errors).every(e => e === null);
 
-            // used for isolating sinks
-            const makeIsolationKey = function (key: string) {
-                const namespace: string = (DOM as any)._namespace[0];
-                const prefix = namespace ? namespace.replace(SCOPE_PREFIX, '') : '';
-                return [prefix, key].filter(Boolean).join('-');
-            }
+                // used for isolating sinks
+                const makeIsolationKey = function(key: string) {
+                    const namespace: string = (DOM as any)._namespace[0];
+                    const prefix = namespace ? namespace.replace(SCOPE_PREFIX, '') : '';
+                    return [prefix, key].filter(Boolean).join('-');
+                };
 
-            const vnodes: Record<keyof Decl, VNode | null> = Object.keys(fields).map<[keyof Decl, VNode | null]>((key: keyof Decl) => {
-                const field: Field<Decl[keyof Decl]> | null | undefined = fields[key];
-                const value: Decl[keyof Decl] = values[key];
+                const vnodes: Record<keyof Decl, VNode | null> = Object.keys(fields)
+                    .map<[keyof Decl, VNode | null]>((key: keyof Decl) => {
+                        const field: Field<Decl[keyof Decl]> | null | undefined = fields[key];
+                        const value: Decl[keyof Decl] = values[key];
 
-                if (!field) {
-                    return [key, null];
-                }
+                        if (!field) {
+                            return [key, null];
+                        }
 
-                // any because the validation result differs from field to field
-                const error: any = errors[key] || null;
+                        // any because the validation result differs from field to field
+                        const error: any = errors[key] || null;
 
-                const vnode = field.view({
-                    error,
-                    touched: touchedKeys.has(key),
-                    value,
-                }, { valid: allValid });
+                        const vnode = field.view(
+                            {
+                                error,
+                                touched: touchedKeys.has(key),
+                                value,
+                            },
+                            { valid: allValid },
+                        );
 
-                return [key, totalIsolateVNode(vnode, makeIsolationKey(key))];
-            }).reduce((acc: Record<keyof Decl, VNode | null>, [key, vnode]: [keyof Decl, VNode]) => Object.assign({}, acc, { [key]: vnode }), {} as Record<keyof Decl, VNode | null>);
+                        return [key, totalIsolateVNode(vnode, makeIsolationKey(key))];
+                    })
+                    .reduce(
+                        (acc: Record<keyof Decl, VNode | null>, [key, vnode]: [keyof Decl, VNode]) =>
+                            Object.assign({}, acc, { [key]: vnode }),
+                        {} as Record<keyof Decl, VNode | null>,
+                    );
 
-            const vnode = renderer(vnodes);
+                const vnode = renderer(vnodes);
 
-            return vnode;
-        }).remember();
+                return vnode;
+            })
+            .remember();
 
         return {
             DOM: vnode$,
@@ -156,11 +187,7 @@ function totalIsolateVNode(node: VNode, scope: string): VNode {
         const prevFullScopeNum = isolateData.replace(/(cycle|\-)/g, '');
         const fullScopeNum = scope.replace(/(cycle|\-)/g, '');
 
-        if (
-            isNaN(parseInt(prevFullScopeNum)) ||
-            isNaN(parseInt(fullScopeNum)) ||
-        prevFullScopeNum > fullScopeNum
-        ) {
+        if (isNaN(parseInt(prevFullScopeNum)) || isNaN(parseInt(fullScopeNum)) || prevFullScopeNum > fullScopeNum) {
             // > is lexicographic string comparison
             return node;
         }
@@ -175,8 +202,10 @@ function totalIsolateVNode(node: VNode, scope: string): VNode {
     return node;
 }
 
-function evolveC<Struct extends object>(transformations: Partial<{ [P in keyof Struct]: Endo<Struct[P]> }>): Endo<Struct> {
-    return function (struct: Struct): Struct {
+function evolveC<Struct extends object>(
+    transformations: Partial<{ [P in keyof Struct]: Endo<Struct[P]> }>,
+): Endo<Struct> {
+    return function(struct: Struct): Struct {
         const newStruct: any = Object.create(null);
 
         Object.keys(struct).forEach((key: keyof Struct) => {
@@ -187,8 +216,8 @@ function evolveC<Struct extends object>(transformations: Partial<{ [P in keyof S
             } else {
                 newStruct[key] = f(struct[key]);
             }
-        })
+        });
 
         return newStruct as Struct;
-    }
-};
+    };
+}
--- src/types.ts
+++ src/types.ts
@@ -14,7 +14,7 @@ export type Sources<Decl extends FormDeclaration<any>> = {
     onion: StateSource<Values<Decl>>;
     renderer$: MemoryStream<FormRenderer<Decl>>;
     validators$?: MemoryStream<ValidatorsFor<Decl>>;
-}
+};
 
 /**
  * Sink streams of a form component.
@@ -23,8 +23,7 @@ export type Sinks<Decl extends FormDeclaration<any>> = {
     DOM: MemoryStream<VNode>;
     onion: Stream<Endo<Values<Decl>>>;
     submission$: Stream<Event>;
-}
-
+};
 
 // form / fields
 
@@ -34,16 +33,16 @@ export type Sinks<Decl extends FormDeclaration<any>> = {
 export type Field<T, Options extends FieldOptions<any> = FieldOptions<string>> = {
     intent: Intent<T>;
     view: View<T, Options>;
-}
+};
 
 /**
  * An `Intent` defines how to change the field value by listening to user's actions.
- * 
+ *
  * It takes a `DOMSource` as an argument and returns a stream of endomorphisms.
  * The value of the field can be modified (or set) through this stream.
- * 
+ *
  * Note that the given `DOMSource` is isolated.
- * 
+ *
  * ```
  * const intent: Intent<string> = (DOM: DOMSource) => DOM.select('input').events('input').map((e: any) => _ => e.target.value);
  * ```
@@ -52,11 +51,14 @@ export type Intent<T> = (DOM: DOMSource) => Stream<Endo<T>>;
 
 /**
  * A `View` defines how to tell the current state of the field to the user.
- * 
+ *
  * It takes a `ViewInput`, which includes the current value, error message and so forth,
  * and converts it into a `VNode`.
  */
-export type View<T, Options extends FieldOptions<any> = FieldOptions<string>> = (input: ViewInput<T, Options>, metaData: MetaData) => VNode;
+export type View<T, Options extends FieldOptions<any> = FieldOptions<string>> = (
+    input: ViewInput<T, Options>,
+    metaData: MetaData,
+) => VNode;
 
 /**
  * A data set represents the current status of a field.
@@ -65,7 +67,7 @@ export type ViewInput<T, Options extends FieldOptions<any> = FieldOptions<string
     error?: Options['error'];
     touched: boolean;
     value: T;
-}
+};
 
 export type FieldOptions<Err> = {
     error: Err;
@@ -75,7 +77,7 @@ export type FieldOptions<Err> = {
  * A helper (type-level) function which lifts the form value type to the form declaration type,
  * assuming every field reports its error with a `string` value.
  */
-export type SimpleForm<Form> = { [FieldName in keyof Form]: FieldDeclaration<Form[FieldName], { error: string; }> };
+export type SimpleForm<Form> = { [FieldName in keyof Form]: FieldDeclaration<Form[FieldName], { error: string }> };
 
 /**
  * A declaration of a single form field.
@@ -88,35 +90,41 @@ export type FieldDeclaration<T, Options extends FieldOptions<any>> = {
 /**
  * Defines what the entire form is look like.
  */
-export type FormDeclaration<Values extends any> = { [FieldName in keyof Values]: FieldDeclaration<Values[FieldName], { error: any; }> };
+export type FormDeclaration<Values extends any> = {
+    [FieldName in keyof Values]: FieldDeclaration<Values[FieldName], { error: any }>
+};
 
 /**
  * Field implementations for the given `FormDeclaration`.
  */
-export type FieldsFor<Form extends FormDeclaration<any>> = { [FieldName in keyof Form]?: Field<Form[FieldName]['type'], FieldOptions<Form[FieldName]['error']>> };
+export type FieldsFor<Form extends FormDeclaration<any>> = {
+    [FieldName in keyof Form]?: Field<Form[FieldName]['type'], FieldOptions<Form[FieldName]['error']>>
+};
 
 /**
  * Validators implementations for the given `FormDeclaration`.
  */
 export type ValidatorsFor<Form extends FormDeclaration<any>> = {
-    [FieldName in keyof Form]?: Form[FieldName]['error'] extends infer Err ? Validator<Form[FieldName]['type'], Err> : never;
+    [FieldName in keyof Form]?: Form[FieldName]['error'] extends infer Err
+        ? Validator<Form[FieldName]['type'], Err>
+        : never
 };
 
 /**
  * Takes a map from field names to the corresponding `VNode`, and returns a `VNode` of the entire form.
- * 
+ *
  * it is a good practice to return a `VNode` of `form` element.
  */
 export type FormRenderer<Form extends FormDeclaration<any>> = (vnodes: Record<keyof Form, VNode | null>) => VNode;
 
 /**
  * Additional data representing the status of the entire form.
- * 
+ *
  * This is actually passed to `View`s as the second argument.
  */
 export type MetaData = {
     valid: boolean; // indicates whether the entire form is valid
-}
+};
 
 /**
  * A type of a component obtained through onion-flavored isolation.
@@ -132,7 +140,6 @@ export type IsolatedForm<Parent, Child> = Component<
  */
 export type Values<Decl extends FormDeclaration<any>> = { [FieldName in keyof Decl]: Decl[FieldName]['type'] };
 
-
 // validator
 
 /**
@@ -144,11 +151,12 @@ export type Validator<T, Err = string> = (value: T) => Err | null;
 /**
  * Extract the `Child` from the `Parent` using its key or a `Lens`.
  */
-export type ZoomIn<Parent extends object, Scope extends Lens<Parent, any> | keyof Parent> =
-    Scope extends Lens<Parent, infer Child> ? Child :
-    Scope extends keyof Parent ? Parent[Scope] :
-    never;
-
+export type ZoomIn<Parent extends object, Scope extends Lens<Parent, any> | keyof Parent> = Scope extends Lens<
+    Parent,
+    infer Child
+>
+    ? Child
+    : Scope extends keyof Parent ? Parent[Scope] : never;
 
 // others
 
